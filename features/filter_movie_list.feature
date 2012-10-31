bb28fec0d5e377f69c8a860934ddb1f54e202dd4
saasbook <saasbook@saasbook.(none)>
2012-10-31 12:24:59 -0700

    working part 1 and 2

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,13 +23,26 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  Given I check the following ratings: PG, R
   # enter step(s) to uncheck all other checkboxes
+  Given I uncheck the following ratings: G, PG-13
   # enter step to "submit" the search form on the homepage
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see "The Incredibles"
+  Then I should see "Raiders of the Lost Ark"
+  Then I should see "The Terminator"
+  Then I should see "When Harry Met Sally"
+  Then I should see "Amelie"
   # enter step(s) to ensure that other movies are not visible
+  Then I should not see "Alladin"
+  Then I should not see "The Help"
+  Then I should not see "Chocolat"
+  Then I should not see "2001: A Space Odyssey"
+  Then I should not see "Chicken Run"
 
 Scenario: no ratings selected
   # see assignment
 
 Scenario: all ratings selected
-  # see assignment
+  Given I check the following ratings: G, PG, PG-13, R
+  Then I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +14,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +25,22 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_array = rating_list.split(/,\s*/)
+
+  rating_array.each do |rating|
+    step %Q{I #{uncheck}check "ratings_#{rating}"}
+  end
+  step %Q{I press "Refresh"}
+    
+end
+
+Then /I should see all of the movies/ do
+  rows = Movie.count()
+  rows.should == 10
+
+  if (rows == 10)
+    Movie.all.each do |movie|
+      step %Q{I should see "#{movie.title}"}
+    end
+  end
 end
